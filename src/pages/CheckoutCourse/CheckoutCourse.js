72a4cf534b4c719fbb803ea6b25d4daedbfd8738
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-12-09 16:46:20 +0600

    fixed some styling issues of checkout form

--- src/pages/CheckoutCourse/CheckoutCourse.js
+++ src/pages/CheckoutCourse/CheckoutCourse.js
@@ -44,89 +44,85 @@ const CheckoutCourse = () => {
         </div>
       </header>
 
-      <section className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col">
-          <div className="text-center">
-            <h3 className="text-3xl font-semibold">Purchase Course</h3>
-          </div>
+      <section className="container mx-auto px-4 py-6 min-h-screen">
+        <h3 className="text-3xl font-semibold text-center mb-8">
+          Purchase Course
+        </h3>
 
-          <div className="card flex-shrink-0  shadow-2xl bg-base-100">
-            <form
-              onSubmit={confirmPurchaseHandler}
-              className="card-body w-full"
-            >
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Full Name</span>
-                </label>
-                <input
-                  type="text"
-                  placeholder="email"
-                  defaultValue={user?.uid ? user?.displayName : ""}
-                  className="input input-bordered"
-                  readOnly={user?.displayName ? true : false}
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  defaultValue={user?.uid ? user?.email : ""}
-                  readOnly={user?.email ? true : false}
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Cel Number</span>
-                </label>
-                <input
-                  type="tel"
-                  placeholder="+880-**********"
-                  className="input input-bordered"
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Course Name</span>
-                </label>
-                <input
-                  type="text"
-                  defaultValue={courseName}
-                  className="input input-bordered"
-                  readOnly
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Amount</span>
-                </label>
-                <input
-                  type="text"
-                  defaultValue={price}
-                  className="input input-bordered"
-                  readOnly
-                />
-              </div>
-              <div className="form-control w-full max-w-xs">
-                <label className="label">
-                  <span className="label-text">Select payment method</span>
-                </label>
-                <select className="select select-bordered">
-                  <option>Bank Account</option>
-                  <option>Mobile Banking</option>
-                  <option>Master Card</option>
-                  <option>Agent Banking</option>
-                </select>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Purchase Now</button>
-              </div>
-            </form>
-          </div>
+        <div className="max-w-xl mx-auto p-6 rounded-lg shadow-2xl bg-base-100">
+          <form onSubmit={confirmPurchaseHandler} className="">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Full Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                defaultValue={user?.uid ? user?.displayName : ""}
+                className="input input-bordered bg-gray-200"
+                readOnly={user?.displayName ? true : false}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="email"
+                className="input input-bordered bg-gray-200"
+                defaultValue={user?.uid ? user?.email : ""}
+                readOnly={user?.email ? true : false}
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Cel Number</span>
+              </label>
+              <input
+                type="tel"
+                placeholder="+880-**********"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Course Name</span>
+              </label>
+              <input
+                type="text"
+                defaultValue={courseName}
+                className="input input-bordered bg-gray-200"
+                readOnly
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Amount</span>
+              </label>
+              <input
+                type="text"
+                defaultValue={price}
+                className="input input-bordered bg-gray-200"
+                readOnly
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Select payment method</span>
+              </label>
+              <select className="select select-bordered">
+                <option>Bank Account</option>
+                <option>Mobile Banking</option>
+                <option>Master Card</option>
+                <option>Agent Banking</option>
+              </select>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Purchase Now</button>
+            </div>
+          </form>
         </div>
       </section>
       <ToastContainer />
